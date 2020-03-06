5820e6e5494ca1a5f62c76dce6c1f1bd6e87f82f
Heejeong Cho <w0424547@nscc.ca>
2020-03-05 23:21:50 -0400

    Update category css on archive template

--- wp-content/themes/twentyseventeen-child/single-archive-post.php
+++ wp-content/themes/twentyseventeen-child/single-archive-post.php
@@ -18,8 +18,8 @@ if ( $query->have_posts() ) :
 <?php
             the_title('<h3><a href="'.get_permalink().'">', '</a></h3>');
 ?>
-            <div>
-                By <?php the_author_posts_link(); ?> on <?php the_time('F jS, Y'); ?>  in <?php the_category(', '); ?> <?php edit_post_link(__('Edit'), ''); ?>
+            <div class="info">
+                By <?php the_author_posts_link(); ?> on <?php the_time('F jS, Y'); ?>  in <span><?php the_category(', '); ?></span> <?php edit_post_link(__('Edit'), ''); ?>
             </div>
 <?php  
             the_content();
@@ -31,8 +31,8 @@ if ( $query->have_posts() ) :
           
             the_title('<h3><a href="'.get_permalink().'">', '</a></h3>');
 ?>
-            <div>
-                By <?php the_author_posts_link(); ?> on <?php the_time('F jS, Y'); ?>  in <?php the_category(', '); ?> <?php edit_post_link(__('Edit'), ''); ?>
+            <div class="info">
+                By <?php the_author_posts_link(); ?> on <?php the_time('F jS, Y'); ?>  in <span><?php the_category(', '); ?></span> <?php edit_post_link(__('Edit'), ''); ?>
             </div>
 <?php  
             the_content();
